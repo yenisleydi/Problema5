d57519012fc438c9213500ad43514b4504f9c6e7
yenisleydi <126813935+yenisleydi@users.noreply.github.com>
2023-05-02 20:07:34 -0600

    Problema 5 actualizado

--- src/NumerosPares.java
+++ src/NumerosPares.java
@@ -7,10 +7,10 @@ public class NumerosPares {
     public void par(){
         Scanner scanner=new Scanner(System.in);
         Random rand = new Random();
-        int numero = rand.nextInt(1000) + 1;
-        if (numero % 2==0){
             System.out.println("Ingresa un numero par de 2 a 1000");
             int num=scanner.nextInt();
+            int numero = rand.nextInt(1000) + 1;
+            if (numero % 2==0){
             if (num==numero){
                 System.out.println(numero);
                 System.out.println("------------------------------------------");
